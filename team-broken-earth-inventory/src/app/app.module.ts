1ca7c040df604dea9bfcca4d311c46088f2fa520
Sam Newhook <newhook.sam@gmail.com>
2019-12-22 15:50:18 -0330

    End of Section 2 Frontend of Udemy course.

--- team-broken-earth-inventory/src/app/app.module.ts
+++ team-broken-earth-inventory/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ItemCreateComponent } from './items/item-create/item-create.component';
-import {  FormsModule  } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HeaderComponent } from './header/header.component'
+import { ItemListComponent } from './items/item-list/item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemCreateComponent
+    ItemCreateComponent,
+    HeaderComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- team-broken-earth-inventory/src/app/items/item-create/item-create.component.ts
+++ team-broken-earth-inventory/src/app/items/item-create/item-create.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import { ItemsService } from '../items.service';
 
 @Component({
   selector: 'app-item-create',
-  templateUrl: './item-create.component.html'
+  templateUrl: './item-create.component.html',
+  styleUrls: ['./item-create.component.css']
 })
 export class ItemCreateComponent {
-  enteredValue = '';
-  newItem = 'NO CONTENT';
+  enteredItemInfo = '';
+  enteredItemTitle = '';
 
-  onAddItem() {
-    this.newItem = this.enteredValue;
+  constructor(public itemsService: ItemsService) {}
+
+  onAddItem(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.itemsService.addItem(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
