d180a06c39818d5ff22cc11f53c3aa93612f3548
Sam Newhook <newhook.sam@gmail.com>
2020-01-05 15:23:06 -0330

    Finished Authentication

--- team-broken-earth-inventory/src/app/app-routing.module.ts
+++ team-broken-earth-inventory/src/app/app-routing.module.ts
@@ -4,18 +4,20 @@ import { ItemListComponent } from './items/item-list/item-list.component';
 import { ItemCreateComponent } from './items/item-create/item-create.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
     { path: '', component: ItemListComponent },
-    { path: 'create', component: ItemCreateComponent },
-    { path: 'edit/:itemId', component: ItemCreateComponent },
+    { path: 'create', component: ItemCreateComponent, canActivate: [AuthGuard] },
+    { path: 'edit/:itemId', component: ItemCreateComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthGuard]
 })
 export class AppRoutingModule {
 
--- team-broken-earth-inventory/src/app/auth/auth.service.ts
+++ team-broken-earth-inventory/src/app/auth/auth.service.ts
@@ -11,6 +11,7 @@ export class AuthService {
     private isAuthenticated = false;
     private token: string;
     private authStatusListener = new Subject<boolean>();
+    private tokenTimer: any;
 
     constructor(private http: HttpClient, private router: Router) {
 
@@ -29,31 +30,82 @@ export class AuthService {
     }
 
     createUser(email: string, password: string) {
-        const authData: AuthData = {email: email, password: password};
+        const authData: AuthData = { email: email, password: password };
         this.http.post("http://localhost:3000/users/signup", authData)
-        .subscribe(response => {
-            console.log(response);
-        });
+            .subscribe(response => {
+                console.log(response);
+            });
     }
 
     login(email: string, password: string) {
-        const authData: AuthData = { email: email, password: password};
-        this.http.post<{token: string}>("http://localhost:3000/users/login", authData)
-        .subscribe(response => {
-            const token = response.token;
-            this.token = token;
-            if (token) {
-                this.isAuthenticated = true;
-                this.authStatusListener.next(true);
-                this.router.navigate(['/']);
-            }
-        });
+        const authData: AuthData = { email: email, password: password };
+        this.http.post<{ token: string, expiresIn: number }>("http://localhost:3000/users/login", authData)
+            .subscribe(response => {
+                const token = response.token;
+                this.token = token;
+                if (token) {
+                    const expiresInDuration = response.expiresIn;
+                    this.setAuthTimer(expiresInDuration);
+                    this.isAuthenticated = true;
+                    this.authStatusListener.next(true);
+                    const now = new Date();
+                    const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+                    this.saveAuthData(token, expirationDate);
+                    this.router.navigate(['/']);
+                }
+            });
+    }
+
+    autoAuthUser() {
+        const authInformation = this.getAuthData();
+        if (!authInformation) {
+            return;
+        }
+        const now = new Date();
+        const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+        if (expiresIn > 0) {
+            this.token = authInformation.token;
+            this.isAuthenticated = true;
+            this.setAuthTimer(expiresIn / 1000);
+            this.authStatusListener.next(true);
+        }
     }
 
     logout() {
         this.token = null;
         this.isAuthenticated = false;
         this.authStatusListener.next(false);
-        this.router.navigate(['/']);
+        clearTimeout(this.tokenTimer);
+        this.clearAuthData();
+        this.router.navigate(['/login']);
+    }
+
+    private saveAuthData(token: string, expirationDate: Date) {
+        localStorage.setItem('token', token);
+        localStorage.setItem('expiration', expirationDate.toISOString());
+    }
+
+    private clearAuthData() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('expiration');
+    }
+
+    private getAuthData() {
+        const token = localStorage.getItem('token');
+        const expirationDate = localStorage.getItem('expiration');
+
+        if (!token || !expirationDate) {
+            return;
+        }
+        return {
+            token: token,
+            expirationDate: new Date(expirationDate)
+        }
+    }
+
+    private setAuthTimer(duration: number) {
+        this.tokenTimer = setTimeout(() => {
+            this.logout()
+        }, duration * 1000);
     }
 }
