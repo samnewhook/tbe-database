b8fc09ba2b781d43386b4dfed1e21bc8429e8e7f
Sam Newhook <newhook.sam@gmail.com>
2020-01-10 13:51:21 -0500

    Optimizing Backend

--- team-broken-earth-inventory/backend/routes/items.js
+++ team-broken-earth-inventory/backend/routes/items.js
@@ -1,42 +1,19 @@
 const express = require("express");
-const multer = require("multer");
 const itemsController = require("../controllers/items")
 
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
-
-const MIME_TYPE_MAP = {
-    'image/png': 'png',
-    'image/jpeg': 'jpg',
-    'image/jpg': 'jpg'
-}
-
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const isValid = MIME_TYPE_MAP[file.mimetype];
-        let error = new Error("invalid mime type");
-        if (isValid) {
-            error = null;
-        }
-        cb(null, "backend/images")
-    },
-    filename: (req, file, cb) => {
-        const name = file.originalname.toLowerCase().split(' ').join('-');
-        const ext = MIME_TYPE_MAP[file.mimetype];
-        cb(null, name + '-' + Date.now() + '.' + ext);
-    }
-});
+const extractFile = require('../middleware/check-mime-type');
 
 router.post("", 
 checkAuth,
-multer({storage: storage}).single("image"),
+extractFile,
 itemsController.createItem
 );
 
 router.put("/:id",
 checkAuth,
-multer({storage: storage}).single("image"), 
+extractFile, 
 itemsController.updateItem);
 
 router.get("", 
