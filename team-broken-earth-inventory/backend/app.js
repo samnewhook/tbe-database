162466850f03285d4a8d0940f3bead4e8f89e24e
Sam Newhook <newhook.sam@gmail.com>
2020-01-24 22:35:10 -0500

    Using cors

--- team-broken-earth-inventory/backend/app.js
+++ team-broken-earth-inventory/backend/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const itemsRoutes = require("./routes/items");
 const userRoutes = require("./routes/user");
@@ -20,18 +21,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use("/images", express.static(path.join("images")));
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers", 
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-        );
-    res.setHeader(
-        "Access-Control-Allow-Methods", 
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-        );
-    next();
-});
+//app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//    res.setHeader(
+//        "Access-Control-Allow-Headers", 
+//        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//        );
+//    res.setHeader(
+//       "Access-Control-Allow-Methods", 
+//        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+//        );
+//    next();
+//});
+
+const corsOptions = {
+    //origin: 'http://sam-tbe-inventory.s3-website.us-east-2.amazonaws.com',
+    origin: true,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+}
+
+app.use(cors(corsOptions));
 
 app.use("/api/items", itemsRoutes);
 app.use("/api/users", userRoutes);
